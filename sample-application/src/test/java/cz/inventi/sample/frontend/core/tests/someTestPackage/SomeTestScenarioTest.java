40c37ca9e9345214c50944ec904a079ae07d0ca0
tomas.fenyk <tomas.fenyk@inventi.cz>
2020-04-29 20:58:57 +0200

    Fixed .gitignore

---
+++ sample-application/src/test/java/cz/inventi/sample/frontend/core/tests/someTestPackage/SomeTestScenarioTest.java
@@ -0,0 +1,12 @@
+package cz.inventi.sample.frontend.core.tests.someTestPackage;
+
+import cz.inventi.sample.frontend.core.tests.RegressionTest;
+import org.testng.annotations.Test;
+
+public class SomeTestScenarioTest extends RegressionTest {
+
+    @Test
+    public void someTest () {
+        // Some test scenario
+    }
+}
