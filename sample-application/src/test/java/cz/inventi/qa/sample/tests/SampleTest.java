61adc809d8d786afd3d7f8d5191c517eb3206ab0
tomas.fenyk <tomas.fenyk@inventi.cz>
2021-05-19 14:39:09 +0200

    Fixed gitignore

---
+++ sample-application/src/test/java/cz/inventi/qa/sample/tests/SampleTest.java
@@ -0,0 +1,23 @@
+package cz.inventi.qa.sample.tests;
+
+import cz.inventi.qa.framework.core.objects.test.TestBase;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+@Epic("Provide Sample Structure for Custom Module")
+@Story("Provide Sample Test for Custom Module")
+public class SampleTest extends TestBase {
+
+    @BeforeClass
+    @Override
+    public void initSteps() {
+    }
+
+    @Test(description = "This Is a Sample Test")
+    public void retrieveDfAccessToken(String userNameDf, String userPasswordDf) {
+        // Write some test here
+    }
+}
