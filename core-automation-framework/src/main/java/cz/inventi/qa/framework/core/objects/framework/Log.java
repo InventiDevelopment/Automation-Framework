9b1d821fe65a2b46564d8aed001de2c6c7827d1d
tomas.fenyk <tomas.fenyk@inventi.cz>
2022-02-09 15:11:38 +0100

    Add AssertJ assertions, refactor assertion exceptions and Maven POMs.

--- core-automation-framework/src/main/java/cz/inventi/qa/framework/core/objects/framework/Log.java
+++ core-automation-framework/src/main/java/cz/inventi/qa/framework/core/objects/framework/Log.java
@@ -1,15 +1,21 @@
 package cz.inventi.qa.framework.core.objects.framework;
 
 import cz.inventi.qa.framework.core.data.enums.RunMode;
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+
 
 public class Log {
     public static final String LOGGER_NAME = "cz.inventi.qa.framework";
 
     public static void info(String text) {
         getLogger().info(text);
+    }
+
+    public static void error(String text) {
+        getLogger().error(text);
     }
 
     public static void warn(String text) {
@@ -31,6 +37,8 @@ public class Log {
             default -> Level.ERROR;
         };
         Log.info("Setting global log level to '" + logLevel + "'");
-        LogManager.getRootLogger().setLevel(logLevel);
+        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        ctx.getConfiguration().getLoggerConfig(LogManager.ROOT_LOGGER_NAME).setLevel(logLevel);
+        ctx.updateLoggers();
     }
 }
--- core-automation-framework/src/main/java/cz/inventi/qa/framework/testapps/framework/steps/LanguageTestSteps.java
+++ core-automation-framework/src/main/java/cz/inventi/qa/framework/testapps/framework/steps/LanguageTestSteps.java
@@ -1,8 +1,8 @@
 package cz.inventi.qa.framework.testapps.framework.steps;
 
 import cz.inventi.qa.framework.core.data.enums.Language;
-import cz.inventi.qa.framework.core.objects.test.steps.StepsBase;
 import cz.inventi.qa.framework.core.objects.test.assertions.Assert;
+import cz.inventi.qa.framework.core.objects.test.steps.StepsBase;
 import cz.inventi.qa.framework.testapps.testweb.lang.Index;
 import cz.inventi.qa.framework.testapps.testweb.webobjects.HomePage;
 import io.qameta.allure.Step;
