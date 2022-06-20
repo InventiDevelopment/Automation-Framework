a554b3d56510244da8d680a5bcb5b939cf41f5e5
tomas.fenyk <tomas.fenyk@inventi.cz>
2022-06-20 17:03:23 +0200

    Rename

---
+++ sample-application/src/test/java/cz/inventi/qa/sampleapplication/tests/SampleTest.java
@@ -0,0 +1,22 @@
+package cz.inventi.qa.sampleapplication.tests;
+
+import cz.inventi.qa.framework.core.objects.test.TestBase;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Epic("Provide Sample Structure for Custom Module")
+@Story("Provide Sample Test for Custom Module")
+public class SampleTest extends TestBase {
+
+    @BeforeClass
+    @Override
+    public void initSteps() {
+    }
+
+    @Test(description = "This Is a Sample Test")
+    public void retrieveDfAccessToken(String userNameDf, String userPasswordDf) {
+        // Write some test here
+    }
+}
